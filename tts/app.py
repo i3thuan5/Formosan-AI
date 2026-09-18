57df838e6decd6c18db2a4b8908ef5039f7d7da1
KUNHANG LEE <kalang@ithuan.tw>
2026-09-18 15:43:53 +0800

    Use silent_info() to make web UX clear

--- tts/app.py
+++ tts/app.py
@@ -130,6 +130,11 @@ DEFAULT_MODEL_ID = list(models_config.keys())[0]
 ETHNICITIES = sorted(set([k.split("_")[0] for k in g2p_object.keys()]))
 
 
+def silent_info(*args, **kwargs):
+    # F5-TTS 內部會用 show_info 跳英文提示，但太干擾故不顯示
+    pass
+
+
 @gpu_decorator
 def infer(
     ref_audio_orig,
@@ -140,7 +145,7 @@ def infer(
     cross_fade_duration=0.15,
     nfe_step=32,
     speed=1,
-    show_info=gr.Info,
+    show_info=silent_info,
 ):
     if not ref_audio_orig:
         gr.Warning("Please provide reference audio.")
